91debe69366ca154f04345d6d73b76ddd4612815
Rahul Derashri <rahul.derashri@gmail.com>
2015-11-27 10:57:30 -0500

    BookingDetails console changes

--- src/com/usage/BookTicket.java
+++ src/com/usage/BookTicket.java
@@ -19,16 +19,18 @@ public class BookTicket {
 		int level = 0;
 		int startLevel = 0;
 		int endLevel = 0;
+		
 		while(true){
 			try {
-				System.out.println("Please select from the options");
+				System.out.println("\n\nPlease select from the options");
 				System.out.println("1 - Book tickets");
 				System.out.println("2 - Confirm tickets");
 				System.out.println("3 - Check seats availability at level");
-				System.out.println("4 - How many are on Hold?");
-				System.out.println("5 - How many are Confirmed?");
-				System.out.println("6 - Reset the Ticket Service System");
-				System.out.println("7 - Exit");
+				System.out.println("4 - Get booking details");
+				System.out.println("5 - How many are on Hold?");
+				System.out.println("6 - How many are Confirmed?");
+				System.out.println("7 - Reset the Ticket Service System");
+				System.out.println("8 - Exit");
 				String userEntry = reader.readLine();
 				
 				switch (userEntry) {
@@ -46,9 +48,9 @@ public class BookTicket {
 					case "2":
 						System.out.println("Please Enter Book Hold ID:");
 						seatHoldId = Integer.parseInt(reader.readLine());
-						System.out.println("Please Enter EmailID:");
-						emailID = reader.readLine();
-						System.out.println(service.reserveSeats(seatHoldId, emailID));
+						//System.out.println("Please Enter EmailID:");
+						//emailID = reader.readLine();
+						System.out.println("Confirmation Code: "+service.reserveSeats(seatHoldId, emailID));
 						break;
 					case "3":
 						System.out.println("Please enter level:");
@@ -56,54 +58,23 @@ public class BookTicket {
 						System.out.println("Number of seats available at "+level+": "+service.numSeatsAvailable(level));
 						break;
 					case "4":
-						System.out.println(SeatsDB.getSizeofHoldQ());
+						System.out.println("Please enter confirmationCode:");
+						//level = Integer.parseInt(reader.readLine());
+						System.out.println(service.getBookingDetails(reader.readLine()));
 						break;
 					case "5":
-						System.out.println(SeatsDB.getSizeofBookingMap());
+						System.out.println(SeatsDB.getSizeofHoldQ());
 						break;
 					case "6":
+						System.out.println(SeatsDB.getSizeofBookingMap());
+						break;
+					case "7":
 						SeatsDB.initializeDatabase();
 						System.out.println("Database Initialized");
 						break;
 					default:
 						System.exit(0);
 				}
-				
-				/*if( userEntry.equalsIgnoreCase("1") ){
-					System.out.println("Please Enter EmailID:");
-					emailID = reader.readLine();
-					System.out.println("Please Enter Number of seats:");
-					numSeats = Integer.parseInt(reader.readLine());
-					System.out.println("Please Enter lowest level:");
-					startLevel = Integer.parseInt(reader.readLine());
-					System.out.println("Please Enter end level:");
-					endLevel = Integer.parseInt(reader.readLine());
-					System.out.println(service.findAndHoldSeats(numSeats, startLevel, endLevel, emailID));
-				}
-				else if( userEntry.equalsIgnoreCase("2") ){
-					System.out.println("Please Enter Book Hold ID:");
-					seatHoldId = Integer.parseInt(reader.readLine());
-					System.out.println("Please Enter EmailID:");
-					emailID = reader.readLine();
-					System.out.println(service.reserveSeats(seatHoldId, emailID));
-				}
-				else if( userEntry.equalsIgnoreCase("3") ){
-					System.out.println("Please enter level:");
-					level = Integer.parseInt(reader.readLine());
-					System.out.println("Number of seats available at "+level+": "+service.numSeatsAvailable(level));
-				}
-				else if( userEntry.equalsIgnoreCase("4") ){
-					System.out.println(SeatsDB.getSizeofHoldQ());
-				}
-				else if( userEntry.equalsIgnoreCase("5") ){
-					System.out.println(SeatsDB.getSizeofBookingMap());
-				}
-				else if( userEntry.equalsIgnoreCase("6") ){
-					SeatsDB.initializeDatabase();
-				}
-				else{
-					System.exit(0);
-				}*/
 				
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
